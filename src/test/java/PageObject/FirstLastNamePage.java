028b0c4eb0c82e070ba0f2ec7ceee6d76bac054f
hatsii <_email@example.com>
2025-01-05 19:13:51 +0300

    1

--- src/test/java/PageObject/FirstLastNamePage.java
+++ src/test/java/PageObject/FirstLastNamePage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
 public class FirstLastNamePage {
     public static final SelenideElement solutionsButton = $(By.xpath("//button[text()='Solutions']"));
     public static final SelenideElement ciCdLink = $(By.xpath("//a[contains(text(),'CI/CD')]"));
@@ -12,7 +14,26 @@ public class FirstLastNamePage {
     public static final SelenideElement firstNameInput = $(By.xpath("//input[@name='first_name']"));
     public static final SelenideElement lastNameInput = $(By.xpath("//input[@name='last_name']"));
 
+    public void stepsCheckLastFirstName(String firstName, String lastName){
+        hoverSolutionButton();
+        clickCiCdButton();
+        clickContactSalesButton();
+        setFirstNameInput(firstName);
+        setLastNameInput(lastName);
+    }
 
+    public void expectedFirstLastNameInput(String expectedFirstName, String expectedLastName){
+        String actualFirstName = firstNameInput.shouldBe(visible).getValue();
+        String actualLastName = lastNameInput.shouldBe(visible).getValue();
+
+        assertThat(actualFirstName)
+                .as("First name does not match the expected value")
+                .isEqualTo(expectedFirstName);
+
+        assertThat(actualLastName)
+                .as("Last name does not match the expected value")
+                .isEqualTo(expectedLastName);
+    }
 
     public void hoverSolutionButton(){
         solutionsButton.hover().shouldBe(visible);
