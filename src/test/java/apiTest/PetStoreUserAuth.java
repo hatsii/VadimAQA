3c64e3753cff101c684f099e80429cb2b5a8040b
hatsii <_email@example.com>
2025-01-10 23:12:32 +0300

    1

--- src/test/java/apiTest/PetStoreUserAuth.java
+++ src/test/java/apiTest/PetStoreUserAuth.java
@@ -6,14 +6,14 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 
 public class PetStoreUserAuth {
+    public static final String BASE_URI = "https://petstore3.swagger.io/api/v3";
     @Test
     public void invalidAuthUser(){
-
         //В этом запросе почему-то проходит авторизация с любыми данными
         given()
                 .accept(ContentType.JSON)
                 .contentType(ContentType.JSON)
-                .baseUri("https://petstore3.swagger.io/api/v3")
+                .baseUri(BASE_URI)
                 .when().get("/user/login?username=tatarkof&password=invalidPassword")
                 .then().log().body().statusCode(400);
     }
